c197f4c4a2efee87ef98449d8d642a5e873db18c
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-10 20:45:49 +0600

    create footer components.

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="clearfix footer-area">
+      <div class="footer-top bg-light text-dark py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-lg-3">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, qui dignissimos voluptatem aperiam voluptate numquam quam consequatur repellat molestias, quisquam magni ipsum accusamus deleniti possimus.</p>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, qui dignissimos voluptatem aperiam voluptate numquam quam consequatur repellat molestias, quisquam magni ipsum accusamus deleniti possimus.</p>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, qui dignissimos voluptatem aperiam voluptate numquam quam consequatur repellat molestias, quisquam magni ipsum accusamus deleniti possimus.</p>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, qui dignissimos voluptatem aperiam voluptate numquam quam consequatur repellat molestias, quisquam magni ipsum accusamus deleniti possimus.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyright-area bg-white text-dark">
+        <div class="container">
+          <div class="text-center py-3">
+            &copy;Copyright 2025. All right reserved by <a href="javascript:void(0)">Rubel Mahmud</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+</template>
