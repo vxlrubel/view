7c52820a36624baadd11553293fee5e8907129b4
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-10 23:42:37 +0600

    update hero section.

--- src/components/HeroArea.vue
+++ src/components/HeroArea.vue
@@ -2,39 +2,71 @@
 <template>
   <section class="clearfix hero-area">
     <div class="container">
-
       <div class="hero-content">
-        <h2>Web Design & Development</h2>
-        <p>Web Design & Development involves crafting visually appealing, responsive, and user-friendly websites tailored to meet diverse needs. From intuitive layouts and innovative designs to robust functionality and seamless performance, it blends creativity with technical expertise to deliver engaging digital experiences that leave a lasting impression."</p>
+        <div class="custom-line-height mb-3">
+            <div class="hero-title fw-bold text-uppercase">Web <span class="text-primary">Development</span></div>
+            <div class="hero-subtitle">Applications & Design</div>
+        </div>
+        <p class="hero-description">We specialize in creating innovative websites, powerful web applications, and stunning designs to bring your digital vision to life. Let us help you build a strong online presence that drives success.</p>
       </div>
     </div>
   </section>
 </template>
 
-
 <style scoped lang="scss">
+  .hero-area{
+    background-image: url('../assets/img/hero-image.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    .container{
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: flex-start;
+      height: calc(100dvh - var(--header-height));
 
-.hero-area{
-  background-image: url(https://img.freepik.com/free-photo/stylish-young-modern-woman-look-amazed-pointing-finger-left-aside-copyspace-showing-promo-text-st_1258-122339.jpg?t=st=1735574630~exp=1735578230~hmac=cda00c8793e39cc55ee544d6d474b22170e2a947fd4fe8907c9594976965383f&w=1800);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: top left;
-  .container{
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: flex-end;
-    height: calc(100dvh - var(--header-height));
-
+    }
   }
-}
-
-.hero-content{
-  width: 100%;
-  max-width: 540px;
-  background-color: hsla(0, 0%, 0%, 0.3);
-  backdrop-filter: blur(5px);
-  padding: 2rem;
-}
-
+  .hero-content{
+    width: 100%;
+    max-width: 470px;
+  }
+  .custom-line-height{
+    line-height: 3rem;
+  }
+  .hero-title{
+    --title-font-size: 2.165rem;
+    font-size: var(--title-font-size);
+    text-shadow: 2px 3px 1px white;
+  }
+  .hero-subtitle {
+    --font-size: 1.8rem;
+    font-size: var(--font-size);
+    text-transform: uppercase;
+    font-weight: 600;
+    color: transparent;
+    background: linear-gradient(to right, var(--color-primary), #EC5E95);
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
+  .hero-description{
+    font-size: 1.225rem;
+  }
+  @media(width < 992px){
+    .hero-description{
+      text-shadow: 1px 1px #a4a4a4;
+      backdrop-filter: blur(5px);
+    }
+  }
+  @media(width >= 768px){
+    .hero-title{
+      --title-font-size: 2.5rem;
+    }
+  }
+  @media(width >= 991px){
+    .hero-title{
+      --title-font-size: 2.8rem;
+    }
+  }
 </style>
