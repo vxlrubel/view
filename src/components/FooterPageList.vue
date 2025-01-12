70c148a34675f2c280c775b0f2e306377be904e2
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-12 23:32:16 +0600

    create pagelist component.

---
+++ src/components/FooterPageList.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul class="list-unstyled p-0 m-0">
+    <li><RouterLink to="/" class="text-decoration-none text-dark text-capitalize">Home</RouterLink></li>
+    <li><RouterLink to="/services" class="text-decoration-none text-dark text-capitalize">services</RouterLink></li>
+    <li><RouterLink to="/portfolio" class="text-decoration-none text-dark text-capitalize">portfolio</RouterLink></li>
+    <li><RouterLink to="/about" class="text-decoration-none text-dark text-capitalize">about</RouterLink></li>
+    <li><RouterLink to="/blog" class="text-decoration-none text-dark text-capitalize">blog</RouterLink></li>
+    <li><RouterLink to="/contact" class="text-decoration-none text-dark text-capitalize">contact</RouterLink></li>
+  </ul>
+</template>
+
+<script setup>
+  import { RouterLink } from 'vue-router';
+</script>
