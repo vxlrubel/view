710c8df856a85350eba0966fcb4e609933f1af32
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-12 23:14:03 +0600

    include social share component and implement the address and also the phone number.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,20 @@
+
 <template>
   <footer class="clearfix footer-area">
       <div class="footer-top bg-light text-dark py-5">
         <div class="container">
           <div class="row">
             <div class="col-sm-6 col-lg-3">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, qui dignissimos voluptatem aperiam voluptate numquam quam consequatur repellat molestias, quisquam magni ipsum accusamus deleniti possimus.</p>
+              <RouterLink to="/" class="footer-logo d-inline-block">
+                <img alt="Vue logo" class="img-fluid" src="@/assets/logo.svg" />
+              </RouterLink>
+              <hr>
+              <div class="mb-3">
+                <i class="small"><span class="fw-bold">Address:</span> Uttara-6, Dhaka-1212</i><br>
+                <i class="small"><span class="fw-bold">Phone:</span> +880 1625-601619</i><br>
+              </div>
+              <h4 class="fs-5 pb-1 border-bottom">Social Share:</h4>
+              <SocialShare />
             </div>
             <div class="col-sm-6 col-lg-3">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, qui dignissimos voluptatem aperiam voluptate numquam quam consequatur repellat molestias, quisquam magni ipsum accusamus deleniti possimus.</p>
@@ -27,3 +37,16 @@
       </div>
     </footer>
 </template>
+
+
+<script setup>
+  import { RouterLink } from 'vue-router';
+  import SocialShare from '@/components/SocialShare.vue'
+</script>
+
+<style lang="scss" scoped>
+  .footer-logo{
+    width: 7.5rem;
+  }
+
+</style>
