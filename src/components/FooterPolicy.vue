350da9ffa95e82d88ec627d315d8f3039e9028e2
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-12 23:43:45 +0600

    create policy component.

---
+++ src/components/FooterPolicy.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="list-unstyled p-0 m-0">
+    <li><RouterLink to="/privacy-policy" class="text-decoration-none text-dark text-capitalize">Privacy Policy</RouterLink></li>
+    <li><RouterLink to="/terms-conditions" class="text-decoration-none text-dark text-capitalize">Terms and Conditions</RouterLink></li>
+    <li><RouterLink to="/refund-policy" class="text-decoration-none text-dark text-capitalize">Refund Policy</RouterLink></li>
+    <li><RouterLink to="/service-provision-policy" class="text-decoration-none text-dark text-capitalize">Service Provision Policy</RouterLink></li>
+    <li><RouterLink to="/cookie-policy" class="text-decoration-none text-dark text-capitalize">Cookie Policy</RouterLink></li>
+  </ul>
+</template>
+
+<script setup>
+  import { RouterLink } from 'vue-router';
+</script>
