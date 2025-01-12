7edf3675d06c1c4c55be2c813766b3a375ebed30
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-12 23:50:47 +0600

    create terms and condition component and register route for this page.

--- src/router/index.js
+++ src/router/index.js
@@ -50,6 +50,12 @@ const router = createRouter({
       component: () => import('../views/policies/PrivacyPolicy.vue'),
       meta: { title: site_title('Privacy policy') },
     },
+    {
+      path: '/terms-conditions',
+      name: 'terms-conditions',
+      component: () => import('../views/policies/TermsConditions.vue'),
+      meta: { title: site_title('Terms Conditions') },
+    },
   ],
 });
 
