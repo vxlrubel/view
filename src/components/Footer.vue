486f2d73b4f987d57b5ab956cf7708ccac7a0122
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-15 00:43:14 +0600

    update footer sectino.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,10 @@
 
 <template>
   <footer class="clearfix footer-area">
-      <div class="footer-top bg-light text-dark py-5">
+      <div class="footer-top text-dark py-5">
         <div class="container">
           <div class="row">
-            <div class="col-sm-6 col-lg-3">
+            <div class="col-sm-6 col-lg-3 mb-4 mb-lg-0">
               <RouterLink to="/" class="footer-logo d-inline-block user-select-none">
                 <img alt="Vue logo" class="img-fluid" src="@/assets/logo.svg" />
               </RouterLink>
@@ -16,12 +16,12 @@
               <h4 class="fs-5 pb-1 border-bottom">Social Share:</h4>
               <SocialShare />
             </div>
-            <div class="col-sm-6 col-lg-3">
+            <div class="col-sm-6 col-lg-3 mb-4 mb-lg-0">
               <h3 class="fs-4 text-capitalize user-select-none">Pages</h3>
               <hr>
               <FooterPageList />
             </div>
-            <div class="col-sm-6 col-lg-3">
+            <div class="col-sm-6 col-lg-3 mb-4 mb-sm-0">
               <h3 class="fs-4 text-capitalize user-select-none">Policy Pages</h3>
               <hr>
               <FooterPolicy />
@@ -37,10 +37,10 @@
           </div>
         </div>
       </div>
-      <div class="copyright-area bg-white text-dark">
+      <div class="copyright-area text-dark">
         <div class="container">
           <div class="text-center py-3">
-            &copy;Copyright 2025. All right reserved by <a href="javascript:void(0)">Rubel Mahmud</a>
+            &copy;Copyright {{ currentYear }}. All right reserved by <a href="javascript:void(0)" class="text-decoration-none text-primary fw-bold">Rubel Mahmud</a>
           </div>
         </div>
       </div>
@@ -54,11 +54,21 @@
   import FooterPageList from '@/components/FooterPageList.vue'
   import FooterPolicy from '@/components/FooterPolicy.vue'
   import Subscribe from '@/components/Subscribe.vue'
+
+  const currentYear = new Date().getFullYear();
 </script>
 
 <style lang="scss" scoped>
-  .footer-logo{
-    width: 7.5rem;
+  .footer-area{
+    .footer-top{
+      background-color: hsl(var(--hue), var(--saturation), 97%);
+    }
+    .footer-logo{
+      width: 7.5rem;
+    }
+    .copyright-area{
+      background-color: hsl(var(--hue), var(--saturation), 93%);
+    }
   }
 
 </style>
