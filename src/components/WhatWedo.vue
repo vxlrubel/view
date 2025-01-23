580a256757ca02ae675d729684497ef549f56fab
Rubel Mahmud <vxlrubel@gmail.com>
2025-01-23 19:03:30 +0600

    update component.

--- src/components/WhatWedo.vue
+++ src/components/WhatWedo.vue
@@ -73,6 +73,7 @@
     transition: box-shadow 0.3s ease-in-out;
     &:hover{
       box-shadow: 0px 48px 96px -24px rgba(0, 0, 0, 0.16);
+      border-color: transparent !important;
     }
   }
 </style>
